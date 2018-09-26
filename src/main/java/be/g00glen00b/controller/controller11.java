629bb01b602c731f34a9727835b531311b197e84
kanwal <kanwal@LAPTOP-3N8LG5FT>
2018-09-26 10:43:17 +0530

    jj

--- src/main/java/be/g00glen00b/controller/controller11.java
+++ src/main/java/be/g00glen00b/controller/controller11.java
@@ -3,7 +3,7 @@ package be.g00glen00b.controller;
 public class controller11 {
 public static void main(String[] args) {
 	System.out.println("hello");
-	System.out.println("good");
+	System.out.println("new change");
 }
 
 }
